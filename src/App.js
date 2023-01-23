f8dc20d13382dece36fc7e26b12354100fe52a3f
dasha <dasha.danilchenko1990@gmail.com>
2023-01-23 13:21:57 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import FirstPage from './components/FirstPage';
+import AboutMy from './components/data/AboutMy';
+import PreviousWorkExperience from './components/data/PreviousWorkExperience';
+import MainLayout from './components/MainLayout';
+import NotFound from './components/NotFound';
+import Contacts from './components/data/Contacts';
+import Skills from './components/data/Skills';
+
 
 function App() {
-  return (
+  return ( <BrowserRouter>
     <div className="App">
-    <FirstPage />
+      <Routes>
+        <Route path="/" element={<MainLayout />}/>
+        <Route path="*" element={<NotFound />}/>
+        <Route path="/skills" element={<Skills />}/>
+        <Route path="/about" element={<AboutMy />}/>
+        <Route path="/previous work" element={<PreviousWorkExperience />}/>
+        <Route path="/contacts" element={<Contacts />}/>
+      </Routes>
     </div>
+  </BrowserRouter>
+  
   );
 }
 
