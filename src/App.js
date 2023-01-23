13354ad18e0a58cd834b2699117ac8a8af188b29
dasha <dasha.danilchenko1990@gmail.com>
2023-01-23 12:31:25 +0200

    add first page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import FirstPage from './components/FirstPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+    <FirstPage />
     </div>
   );
 }
